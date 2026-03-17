a1596bdca602d1373ddaa801ff5b77de7eb6d182
Akrity Gupta <akritygupta442261@gmail.com>
2026-03-17 17:01:17 +0530

    first commit

---
+++ Message.java
@@ -0,0 +1,18 @@
+
+
+class DisplayMessage{
+	void displayInfo() {
+		System.out.println("Welcome to java8+");
+	}
+}
+
+
+public class Message {
+	
+	public static void main(String[] args) {
+		DisplayMessage dm=new DisplayMessage();
+		dm.displayInfo();
+		
+	}
+
+}
