b63dc903901f5ef2460147a79db65cb28b6ed2f3
Akrity Gupta <akritygupta442261@gmail.com>
2026-03-22 18:50:39 +0530

    Removed practiceques submodule

---
+++ mapStream/mapPractice11.java
@@ -0,0 +1,16 @@
+package mapStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class mapPractice11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
